73cec058766262ec20ff2d6e7f4b5d5de457ca64
Alex <alex@milyutin.work>
2023-06-26 18:06:19 +0200

    Feature: more flex bootstrap

--- src/boot/register.js
+++ src/boot/register.js
@@ -18,29 +18,46 @@ import {datadogRum} from '@datadog/browser-rum';
 
 export default boot(({app}) => {
     try {
-        if (!process || !process.env || !process.env.DATADOG_APPLICATION_ID) {
-            console.warn('no config for @datadog/browser-rum found');
+        let config = {};
+        if (app.config.globalProperties && app.config.globalProperties.$q.config.DATADOG_APPLICATION_ID) {
+            config = {
+                applicationId: app.config.globalProperties.$q.config.DATADOG_APPLICATION_ID,
+                clientToken: app.config.globalProperties.$q.config.DATADOG_CLIENT_TOKEN,
+                site: app.config.globalProperties.$q.config.DATADOG_SITE,
+                service: app.config.globalProperties.$q.config.DATADOG_SERVICE || undefined,
+                env: app.config.globalProperties.$q.config.DATADOG_ENV || undefined, //'production'
+                version: app.config.globalProperties.$q.config.DATADOG_VERSION || '1.0.0', //'1.0.0'
+                sessionSampleRate: app.config.globalProperties.$q.config.DATADOG_SESSION_SAMPLE_RATE || 100,
+                sessionReplaySampleRate: app.config.globalProperties.$q.config.DATADOG_SESSION_REPLAY_SAMPLE_RATE || undefined, // if not included, the default is 100
+                trackResources: true,
+                trackLongTasks: true,
+                trackUserInteractions: true,
+                allowedTracingUrls: app.config.globalProperties.$q.config.DATADOG_ALLOWED_TRACING_URLS || undefined,
+            }
+        } else if (process && process.env && process.env.DATADOG_APPLICATION_ID) {
+            config = {
+                applicationId: process.env.DATADOG_APPLICATION_ID,
+                clientToken: process.env.DATADOG_CLIENT_TOKEN,
+                site: process.env.DATADOG_SITE,
+                service: process.env.DATADOG_SERVICE || undefined,
+                env: process.env.DATADOG_ENV || undefined, //'production'
+                version: process.env.DATADOG_VERSION || '1.0.0', //'1.0.0'
+                sessionSampleRate: process.env.DATADOG_SESSION_SAMPLE_RATE || 100,
+                sessionReplaySampleRate: process.env.DATADOG_SESSION_REPLAY_SAMPLE_RATE || undefined, // if not included, the default is 100
+                trackResources: true,
+                trackLongTasks: true,
+                trackUserInteractions: true,
+                allowedTracingUrls: process.env.DATADOG_ALLOWED_TRACING_URLS || undefined,
+            }
+        } else {
+            console.warn('[quasar-datadog-boot] no config for @datadog/browser-rum found');
             return;
-        }
-        const config = {
-            applicationId: process.env.DATADOG_APPLICATION_ID,
-            clientToken: process.env.DATADOG_CLIENT_TOKEN,
-            site: process.env.DATADOG_SITE,
-            service: process.env.DATADOG_SERVICE || undefined,
-            env: process.env.DATADOG_ENV || undefined, //'production'
-            version: process.env.DATADOG_VERSION || '1.0.0', //'1.0.0'
-            sessionSampleRate: process.env.DATADOG_SESSION_SAMPLE_RATE || 100,
-            sessionReplaySampleRate: process.env.DATADOG_SESSION_REPLAY_SAMPLE_RATE || undefined, // if not included, the default is 100
-            trackResources: true,
-            trackLongTasks: true,
-            trackUserInteractions: true,
-            allowedTracingUrls: process.env.DATADOG_ALLOWED_TRACING_URLS || undefined,
         }
         datadogRum.init(config);
         datadogRum.startSessionReplayRecording();
 
         app.$dd = datadogRum;
     } catch (e) {
-        console.error('@datadog/browser-rum init error', e);
+        console.error('[quasar-datadog-boot] @datadog/browser-rum init error', e);
     }
 });
